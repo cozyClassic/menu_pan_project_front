bd5f0ea2ee317b134b22e5b18e623224a52f93d9
KIM <km0192@naver.com>
2022-09-07 16:11:23 +0900

    feat: add fetch

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
-import {useEffecft, useState} from "react";
+import {useEffect, useState} from "react";
 
 import AdminBox from './admin/AdminBox';
 
 import "./App.css"
 
-const MOCK1 = [
-  {
-  name : "스타벅스",
-  banner_url : "https://www.naver.com",
-  banner_link : "https://www.naver.com"
-  },
-  {
-    name : "한솥도시락",
-    banner_url : "https://www.naver.com",
-    banner_link : "https://www.naver.com"
-    }
-]
-
 function App() {
   const [loading, setLoading] = useState(true);
-  const [mock, setMock] = useState("");
-  // 과제2 : fetch 사용 위해서 비동기로 처리하기
+  const [data, setData] = useState([]);
 
-  const getMock = async() => {
+  const getData = async() => {
     const path = window.location.pathname;
     const API = `http://localhost:8000/admin${path}`;
     const response = await fetch(API);
     const json = await response.json();
-    setMock(json);
+    setData([...json.data]);
     setLoading(false);
   }
+
+  useEffect(() => {
+    getData();
+  }, []);
   
   return (
     <div className="App">
-    {loading ? (
       <div>
         <h1> APP! </h1>
-        {MOCK1.map((_mock) =>  
-          <AdminBox key={_mock.name} data={_mock} />)}
-      </div>
-    ): (
+    {loading ? (
       <h1> Loading ... </h1>
+    ): (
+      data.map((_data) =>  
+      <AdminBox key={_data.name} data={_data} />)
     )}
+      </div>
     </div>
   );
 }
